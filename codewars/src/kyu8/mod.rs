4fc253b121aeb0a653a11157984d126b17d512f2
amalhanaja <amalhanaja@gmail.com>
2023-04-21 20:00:47 +0700

    add codewars Third Angle of a Triangle solution

--- codewars/src/kyu8/mod.rs
+++ codewars/src/kyu8/mod.rs
@@ -2,5 +2,6 @@ mod grasshopper_messi_goals_function;
 mod remove_first_and_last_character;
 mod reversed_words;
 mod the_if_function;
+mod third_angle_of_a_triangle;
 mod volume_of_a_cuboid;
 mod you_cant_code_under_pressure_1;

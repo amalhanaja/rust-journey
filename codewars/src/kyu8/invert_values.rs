89b53ab9cfbb24cf5f0f750d8506cefa5611d78b
amalhanaja <amalhanaja@gmail.com>
2024-02-19 20:37:56 +0700

    [CW][KYU-8]: Invert values

---
+++ codewars/src/kyu8/invert_values.rs
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad/
+fn invert(values: &[i32]) -> Vec<i32> {
+    values.iter().map(|item| -item).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::invert;
+
+    #[test]
+    fn basic_tests() {
+        assert_eq!(invert(&vec![1, 2, 3, 4, 5]), vec![-1, -2, -3, -4, -5]);
+        assert_eq!(invert(&vec![1, -2, 3, -4, 5]), vec![-1, 2, -3, 4, -5]);
+        assert_eq!(invert(&vec![]), vec![]);
+    }
+}

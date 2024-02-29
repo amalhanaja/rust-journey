2ed7cebb5e6108fd23ec7c0aa3d3e3969f8bdea7
amalhanaja <amalhanaja@gmail.com>
2024-02-29 21:10:03 +0700

    [CW][KYU-8]: DNA to RNA Conversion

--- codewars/src/kyu8/mod.rs
+++ codewars/src/kyu8/mod.rs
@@ -13,6 +13,7 @@ mod convert_boolean_values_to_strings;
 mod convert_number_to_reversed_array_of_digits;
 mod count_of_positives_sum_of_negatives;
 mod counting_sheep;
+mod dna_to_rna_conversion;
 mod even_or_odd;
 mod fake_binary;
 mod find_the_smallest_integer_in_the_array;

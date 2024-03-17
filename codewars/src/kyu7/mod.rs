4d25c8c2fb810cea5c9760d77a4585d1ac9ae5ee
amalhanaja <amalhanaja@gmail.com>
2024-03-17 11:50:05 +0700

    [CW][KYU-7]: Remove exclamation marks

--- codewars/src/kyu7/mod.rs
+++ codewars/src/kyu7/mod.rs
@@ -18,6 +18,7 @@ mod odd_or_even;
 mod ones_and_zeros;
 mod printer_errors;
 mod regex_validate_pin_code;
+mod remove_exclamation_marks;
 mod reverse_words;
 mod shortest_word;
 mod square_every_digit;

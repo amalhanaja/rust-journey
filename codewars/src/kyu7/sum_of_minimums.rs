7da2df6ff5569eafe985fc022e309770bf52fa14
amalhanaja <amalhanaja@gmail.com>
2024-03-22 23:20:26 +0700

    [CW][KYU-7]: Sum of Minimums!

---
+++ codewars/src/kyu7/sum_of_minimums.rs
@@ -0,0 +1,26 @@
+// https://www.codewars.com/kata/5d5ee4c35162d9001af7d699
+
+fn sum_of_minimums(numbers: [[u8; 4]; 4]) -> u8 {
+    numbers.iter().map(|row| row.iter().min().unwrap()).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sum_of_minimums() {
+        assert_eq!(
+            sum_of_minimums([[7, 9, 8, 6], [6, 5, 4, 3], [5, 7, 4, 5], [7, 9, 4, 3]]),
+            16
+        );
+        assert_eq!(
+            sum_of_minimums([[7, 9, 8, 6], [6, 5, 4, 3], [5, 7, 4, 5], [7, 9, 4, 4]]),
+            17
+        );
+        assert_eq!(
+            sum_of_minimums([[7, 9, 8, 84], [6, 5, 4, 65], [5, 7, 4, 23], [7, 9, 4, 25]]),
+            19
+        );
+    }
+}
